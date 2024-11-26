90829c758f69444e2d79713130bdf3bc1750afa2
LVeraPizza <veinticinco95@hotmail.com>
2024-11-26 11:43:23 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/promociones.html',
+        '/productos-ventas.html',
+
+        // Agrega otros archivos que necesites en el caché
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
